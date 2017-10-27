58d3056791be41cd1ec56de295ae51c671d22abf
Cédric Bodet <cedric.bodet@tngtech.com>
2017-10-27 13:20:58 +0200

    Moved coordinate transformations to other file

---
+++ src/utils.h
@@ -0,0 +1,18 @@
+//
+// Created by Cédric Bodet on 27.10.17.
+//
+
+#ifndef PATH_PLANNING_UTILS_H
+#define PATH_PLANNING_UTILS_H
+
+#include <vector>
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+std::vector<double> getFrenet(double x, double y, double theta, const std::vector<double> &maps_x, const std::vector<double> &maps_y);
+
+// Transform from Frenet s,d coordinates to Cartesian x,y
+std::vector<double> getXY(double s, double d, const std::vector<double> &maps_s, const std::vector<double> &maps_x, const std::vector<double> &maps_y);
+
+
+
+#endif //PATH_PLANNING_UTILS_H
